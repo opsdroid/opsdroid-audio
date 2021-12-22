a91e8c7cf6179ad137af917a0bddcd2376e34a1c
Akshat Kumar Singh <akshat.unt@gmail.com>
2021-12-22 20:42:36 +0530

    Update recognizers.py

--- opsdroidaudio/recognizers.py
+++ opsdroidaudio/recognizers.py
@@ -1,36 +1,17 @@
 """Speech to text recognizer functions."""
-import logging
-
-from speech_recognition import Recognizer, AudioData, UnknownValueError
-
-
-_LOGGER = logging.getLogger(__name__)
-
-
-def google_cloud(config, data, sample_rate):
-    """Perform speech recognition using Google Cloud."""
-    audio_data = AudioData(data, sample_rate, 2)
-    try:
-        text = Recognizer().recognize_google_cloud(audio_data,
-                                                   credentials_json=None,
-                                                   language="en-GB",
-                                                   preferred_phrases=None,
-                                                   show_all=False)
-    except UnknownValueError:
-        text = ""
-        _LOGGER.warning("No speech found in audio.")
-    return text
-
-
-def sphinx(config, data, sample_rate):
-    """Perform speech recognition using sphinx."""
-    audio_data = AudioData(data, sample_rate, 2)
-    try:
-        text = Recognizer().recognize_sphinx(audio_data,
-                                             language="en-US",
-                                             keyword_entries=None,
-                                             show_all=False)
-    except UnknownValueError:
-        text = ""
-        _LOGGER.warning("No speech found in audio.")
-    return text
+H='No speech found in audio.'
+G=False
+E=None
+import logging as F
+from speech_recognition import Recognizer as A,AudioData as B,UnknownValueError as C
+D=F.getLogger(__name__)
+def I(config,data,sample_rate):
+	I=B(data,sample_rate,2)
+	try:F=A().recognize_google_cloud(I,credentials_json=E,language='en-GB',preferred_phrases=E,show_all=G)
+	except C:F='';D.warning(H)
+	return F
+def J(config,data,sample_rate):
+	I=B(data,sample_rate,2)
+	try:F=A().recognize_sphinx(I,language='en-US',keyword_entries=E,show_all=G)
+	except C:F='';D.warning(H)
+	return F
